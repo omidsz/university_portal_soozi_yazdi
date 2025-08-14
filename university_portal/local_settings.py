9281915a9b9066dcfe51772bd2b46501dc07cb49
omid <omidszce@gmail.com>
2025-08-15 02:37:15 +0330

    ignore gitignore

---
+++ university_portal/local_settings.py
@@ -0,0 +1,8 @@
+SECRET_KEY = 'django-insecure-3vfi7wdp!1wbgm0ksykkm3hpa)%m*&@h@5e%fm0#6555$dfc_+'
+DEBUG = True
+
+DB_NAME = 'university_portal'
+DB_PASSWORD = 'university_portal'
+DB_HOST = 'localhost'
+DB_USER = 'university_portal'
+DB_PORT = 5432
