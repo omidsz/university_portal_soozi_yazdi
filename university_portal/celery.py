ee84cf81350be09664759ee0bf87d17cbf78d702
omid <omidszce@gmail.com>
2025-08-17 10:45:49 +0330

    change celery settings

--- university_portal/celery.py
+++ university_portal/celery.py
@@ -1,8 +1,26 @@
+# import os
+# from celery import Celery
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'university_portal.settings')
+#
+# app = Celery('university_portal')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.autodiscover_tasks()
+
+
+
+
+# proj/proj/celery.py
 import os
 from celery import Celery
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'university_portal.settings')
 
 app = Celery('university_portal')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
 app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
--- university_portal/settings.py
+++ university_portal/settings.py
@@ -175,10 +175,15 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+# CELERY_ACCEPT_CONTENT = ['json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_TIMEZONE = 'Asia/Tehran'
+CELERY_BROKER_URL = 'redis://:DnWn4VgLYVOUnrjGVtW6Ui7s@redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://:DnWn4VgLYVOUnrjGVtW6Ui7s@redis:6379/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Asia/Tehran'
